fdc42b15a9101b74fd10f16b04f845aaef7fcdd8
nd319R0 <ndzinfor@gmail.com>
2022-03-16 03:00:32 +0100

    Accessing data from forms

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,6 +1,7 @@
 
 <h1>User Login</h1>
     <form>
+        @csrf
         <input type="text" name="user" placeholder="enter name"> <br> <br>
         <input type="text" name="user" placeholder="enter name"> <br> <br>
         <button type="submit">Login</button>
