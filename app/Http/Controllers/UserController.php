4a0bd11bfed23736020f29f76d45e804a845a341
nd319R0 <ndzinfor@gmail.com>
2022-03-18 11:34:38 +0100

    displaying data to a view from a model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\Models\User;
 class UserController extends Controller
 {
     //
-    function index()
+    function getData()
     {
-       return DB::select('select * from users');
+       return user::all();
     }
 }
